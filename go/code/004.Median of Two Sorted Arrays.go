b8861e832d9e753ab906ec290015f30673c4d0aa
huoyinghui <2285020853@qq.com>
2022-07-29 11:42:00 +0800

    add: 004 merge

--- go/code/004.Median of Two Sorted Arrays.go
+++ go/code/004.Median of Two Sorted Arrays.go
@@ -1,21 +1,34 @@
 package code
 
-import (
-	"sort"
-)
+func merge(nums1 []int, nums2 []int) (nums []int) {
+	for low1, low2 := 0, 0; low1 < len(nums1) || low2 < len(nums2); {
+		if low1 >= len(nums1) {
+			nums = append(nums, nums2[low2:]...)
+			return
+		} else if low2 >= len(nums2) {
+			nums = append(nums, nums1[low1:]...)
+			return
+		}
+		if nums1[low1] < nums2[low2] {
+			nums = append(nums, nums1[low1])
+			low1++
+		} else {
+			nums = append(nums, nums2[low2])
+			low2++
+		}
+	}
+	return
+}
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	nums := append([]int{}, nums1...)
-	nums = append(nums, nums2...)
-	sort.Ints(nums)
+	//nums := append([]int{}, nums1...)
+	//nums = append(nums, nums2...)
+	//sort.Ints(nums)
+	nums := merge(nums1, nums2)
 	//fmt.Println(nums)
 	n := len(nums)
 	if n%2 == 0 {
-		idx := n / 2
-		return float64(nums[idx-1]+nums[idx]) / 2
-
-	} else {
-		idx := n / 2
-		return float64(nums[idx])
+		return float64(nums[n/2]+nums[(n/2-1)]) / 2
 	}
+	return float64(nums[n/2])
 }
