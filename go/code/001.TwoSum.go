94468763cc2f0ce218fb3425faf73d6fc5a5f748
huoyinghui <2285020853@qq.com>
2022-08-02 11:26:13 +0800

    add: 双指针

--- go/code/001.TwoSum.go
+++ go/code/001.TwoSum.go
@@ -1,5 +1,7 @@
 package code
 
+import "fmt"
+
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for k, v := range nums {
@@ -7,6 +9,22 @@ func twoSum(nums []int, target int) []int {
 			return []int{idx, k}
 		}
 		m[v] = k
+	}
+	return nil
+}
+
+func twoSumList(ansMap map[string]bool, nums []int, target int) []int {
+	m := make(map[int]bool)
+	ans := make([][]int, 0)
+	for _, a := range nums {
+		b := target - a
+		if _, ok := m[b]; ok {
+			k := fmt.Sprintf("%v+%v=%v", a, b, target)
+			fmt.Printf("%v+%v=%v", a, b, target)
+
+			ans = append(ans, []int{a, b})
+		}
+		m[a] = true
 	}
 	return nil
 }
--- py/code/001. Two Sum.py
+++ py/code/001. Two Sum.py
@@ -12,9 +12,40 @@ class Solution:
             idx_map[v] = idx
         return
 
+    def twoSumV2(self, nums: List[int], target: int) -> List[int]:
+        """
+        使用双指针优化
+        :param nums:
+        :param target:
+        :return:
+        """
+        nums.sort()
+        left, right = 0, len(nums)-1
+        ans = list()
+        while left < right:
+            t = nums[left] + nums[right]
+            if target == t:
+                ans.append([nums[left], nums[right]])
+                while nums[left] == nums[left+1]:
+                    left += 1
+                while nums[right] == nums[right-1]:
+                    right -= 1
+                left += 1
+                right -= 1
+            elif t > target:
+                right -= 1
+            else:
+                left += 1
+        return ans
+
 
 def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    s = Solution().twoSumV2(nums, target)
+    print(s)
     pass
+    assert s == [[2, 7]]
 
 
 if __name__ == '__main__':
